fc8f2aef372f5930eb9d93143304f9c8ebea982c
zaknafien99 <67056163+zaknafien99@users.noreply.github.com>
2020-10-14 11:05:16 +1000

    /?

---
+++ source/repos/Tree/Tree/Tree.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef TREE_H
+#define TREE_H
+#include <iostream>
+
+class Tree {
+	int height;
+public:
+	Tree(int treeHeight) : height(treeHeight) {}
+	~Tree() { std::cout << "*"; }
+	friend std::ostream& operator<<(std::ostream& os, const Tree* t) {
+		return os << "Tree height is: "
+			<< t->height << std::endl;
+	}
+};
+#endif //TREE_H ///:~
