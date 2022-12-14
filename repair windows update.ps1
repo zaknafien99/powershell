bbdee582dff0e40862de1c9950229da8b6372905
zaknafien99 <67056163+zaknafien99@users.noreply.github.com>
2022-12-15 07:03:51 +1000

    Add files via upload
    This script will solve the problem of repeated failures of windows updates to install. It does this by renaming the distribution folders, restarting key services, checking the health of the windows installation repairing corrupt files.

---
+++ repair windows update.ps1
@@ -0,0 +1,13 @@
+﻿# This script renames the software distribution folders, after running this try windows update again
+
+stop-service -Name bits
+Stop-Service -Name wuauserv
+Rename-Item %systemroot%\softwaredistribution softwaredistribution.bak
+Rename-Item %systemroot%\system32\catroot2 catroot2.bak
+Start-Service -Name BITS
+Start-Service -Name wuauserv
+
+DISM.exe /Checkhealth
+
+#use windows update to repair corrupt files.
+DISM.exe /Online /Cleanup-image /Restorehealth
